05da64ec840610a529d005fe5d476f57f64dc6e0
allicsander <me@evergoal.ru>
2020-04-04 18:36:56 +0300

    async with timeout

---
+++ yes.js
@@ -0,0 +1,14 @@
+const fetchData = callback  => { 
+    setTimeout(() => callback('done!'), 1500 ); 
+} ;
+
+
+setTimeout(() => {
+    console.log('timer is done!');
+    fetchData(text => {
+        console.log(text);
+    });
+}, 2000);
+
+console.log('Hi!');
+console.log('hello!');
