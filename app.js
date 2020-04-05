b77712dba958d343b880cb242d3f1083f2a99ae1
allicsander <me@evergoal.ru>
2020-04-05 19:32:05 +0300

    going express

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const http = require('http');
 
+const express = require('express');
 
-const server = http.createServer( (req, res) => {
-   console.log(req);
-  // process.exit();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html><head><title>Yummy stuff</title></head>');
-  res.write('<body><p>hi</p></body>');
-  res.write('</html>');
-  res.end();
+const app = express();
 
-} );
+const server = http.createServer(app);
 
 server.listen(3000);
+
+
