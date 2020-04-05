7aa43341a879db43926f30e085efdccbe2ac731b
allicsander <me@evergoal.ru>
2020-04-05 20:09:23 +0300

    make a response

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use((req, res, goOn) => {
     console.log('first middleware!');
-    next();
+    goOn();
 });
 
 app.use((req, res, next) => {
    console.log('second middleware!'); 
+   res.send('<h1> yummy stuff </h1>');
 });
 
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
 
 
